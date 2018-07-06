3a98906fde8b14efbec4754aea1713fc1aa9d507
Prashant Mahesvara <lord_prashant@hotmail.com>
2018-07-07 01:17:30 +0200

    First commit

---
+++ Mosh.Vidly/Mosh.Vidly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mosh.Vidly.MvcApplication" Language="C#" %>
---
+++ Mosh.Vidly/Mosh.Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mosh.Vidly.Startup))]
+namespace Mosh.Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
